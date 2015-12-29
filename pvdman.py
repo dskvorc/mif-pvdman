127c4a99199d8f52f255321d5140d034b405726e
Dejan Skvorc <dejan.skvorc@gmail.com>
2015-12-29 20:33:37 +0100

    a sketch up implementation of the PvD manager

--- pvdman.py
+++ pvdman.py
@@ -1,1 +1,28 @@
+class PvdRecord:
+  def __init__(self, pvdId, pvdInfo, netnsName):
+    self.pvdId = pvdId
+    self.pvdInfo = pvdInfo
+    self.netnsName = netnsName
 
+
+class PvdManager:
+  pvds = {}
+  __netnsIdGenerator = 0;
+
+  
+  def __getNetnsName():
+    __netnsIdGenerator += 1;
+    return 'mif-pvd-' + str(__netnsIdGenerator)
+
+    
+  def setPvd(ifaceName, pvdInfo):
+    if (pvds[pvdInfo.pvdId] is None):
+      pvd = PvdRecord(pvdInfo.pvdId, pvdInfo, __getNetnsName())
+      pvds[pvd.pvdId] = pvd
+      # TODO: create namespace and configure PvD parameters on a given interface
+    else
+      raise Exception('PvD duplicate error: PvD with given ID is already configured.')
+
+      
+  def removePvd(ifaceName, pvdId):
+    # TBD
