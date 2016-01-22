54bd38d111dbe93474363f981540fc1150309639
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2016-01-22 21:32:40 +0100

    merged with client API (skeleton)

--- main.py
+++ main.py
@@ -8,28 +8,10 @@ import argparse
 
 from pvdman import PvdManager
 from ndpclient import NDPClient
-
-
-class PvdApiServer ( dbus.service.Object ):
-	''' TODO '''
-	def __init__(self):
-		# using system bus - put pvd-man.conf file into /etc/dbus-1/system.d/
-		bus_name = dbus.service.BusName('org.freedesktop.PvDManager', bus=dbus.SystemBus())
-		dbus.service.Object.__init__(self, bus_name, '/org/freedesktop/PvDManager')
-
-	@dbus.service.method('org.freedesktop.PvDManager')
-	def getAllPvDs(self):
-		print ( "Got request!" )
-		return ["Hello", "from", "PvDManager"]
-
-	# on client side for dbus:
-	# bus = dbus.SystemBus()
-	# pvdApiClient = bus.get_object('org.freedesktop.PvDManager', '/org/freedesktop/PvDManager')
-	# getAllPvDs = pvdApiClient.get_dbus_method('getAllPvDs', 'org.freedesktop.PvDManager')
-	# print ( str(getAllPvDs()) )
-
+from pvdserver import PvdApiServer
 
 def ndp_pending ( fd, cond, ndpc, pvdman ):
+	''' Process received RAs '''
 	pvdinfos = ndpc.get_pvdinfo()
 	if pvdinfos:
 		for ( iface, pvdInfo ) in pvdinfos:
@@ -44,14 +26,14 @@ if __name__ == "__main__":
 	parser.add_argument ( '-u', required=False, type=str, help='PvD identifier to ask from radvd', dest='uuid' )
 	arg = parser.parse_args()
 
-	# main loop, dbus initialization
-	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-	apiDBUSResponder = PvdApiServer()
-	print ( "PvdApiServer Initialized" )
-
 	# create pvdmanager control object
 	pvdman = PvdManager()
 	print ( "PvdManager Initialized" )
+
+	# main loop, dbus initialization
+	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+	apiDBUSResponder = PvdApiServer(pvdman)
+	print ( "PvdApiServer Initialized" )
 
 	# create ndpclient object and register socket for events
 	ndpc = NDPClient( iface = arg.iface )
